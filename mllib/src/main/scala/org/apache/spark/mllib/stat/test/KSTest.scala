ea5dcf5c54ccfabc4a534401bf635da57f9de953
jose.cambronero <jose.cambronero@cloudera.com>
2015-06-26 16:16:31 -0700

    added 2 sample ks scaladocs comments

--- mllib/src/main/scala/org/apache/spark/mllib/stat/test/KSTest.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/stat/test/KSTest.scala
@@ -189,53 +189,80 @@ private[stat] object KSTest {
     new KSTestResult(pval, ksStat, NullHypothesis.oneSampleTwoSided.toString)
   }
 
-  // start of 2 sample functions
+  /**
+   *  Implements a 2 sample, 2 sided Kolmogorov Smirnov test
+   * @param data1 `RDD[Double]` first sample of data
+   * @param data2 `RDD[Double]` second sample of data
+   * @return A KSTestResult with the test statistic, p-value (calculated with the 2 sample sizes),
+   *         and appropriate null hypothesis
+   */
   def testTwoSamples(data1: RDD[Double], data2: RDD[Double]): KSTestResult = {
     val n1 = data1.count().toDouble
     val n2 = data2.count().toDouble
-    val isSample1 = true // we need a way to identify them once co-sorted
-    // combine samples identified samples
+    val isSample1 = true // identifier for sample 1, needed after co-sort
+    // combine identified samples
     val joinedData = data1.map(x => (x, isSample1)) ++ data2.map(x => (x, !isSample1))
     // co-sort and operate on each partition
     val localData = joinedData.sortBy(x => x).mapPartitions { part =>
-      searchTwoSampleCandidates(part, n1, n2) // local extrema
-      }.collect()
+    searchTwoSampleCandidates(part, n1, n2) // local extrema
+    }.collect()
     val ksStat = searchTwoSampleStatistic(localData, n1 * n2) // result: global extreme
     evalTwoSampleP(ksStat, n1.toInt, n2.toInt)
   }
 
-  // TODO: not sure if we want to break this up into 2 functions how we did with 1 sample??
+  /**
+   * Calculates maximum distance candidates and counts from each sample  within one partition for
+   * the 2 sample Kolmogorov Smirnov test implementation
+   * @param partData `Iterator[(Double, Boolean)]` the data in 1 partition of the co-sorted RDDs,
+   *                each element is additionally tagged with a boolean flag for sample 1 membership
+   * @param n1 `Double` sample 1 size
+   * @param n2 `Double` sample 2 size
+   * @return `Iterator[(Double, Double, Double)]` where the first element is an unadjusted minimum
+   *        distance , the second is an unadjusted maximum distance (both of which will later
+   *        be adjusted by a constant to account for elements in prior partitions), and a
+   *        count corresponding to the numerator of the adjustment constant coming from this
+   *        partition
+   */
   private def searchTwoSampleCandidates(
       partData: Iterator[(Double, Boolean)],
       n1: Double,
       n2: Double)
     : Iterator[(Double, Double, Double)] = {
-    // local minimum, local maximum, index for sample1/sample2
+    // fold accumulator: local minimum, local maximum, index for sample1/sample2
     case class KS2Acc(min: Double, max: Double, ix1: Int, ix2: Int)
     val initAcc = KS2Acc(Double.MaxValue, Double.MinValue, -1, -1)
     // traverse the data in partition and calculate distances and counts
     val results = partData.foldLeft(initAcc) { case (acc: KS2Acc, (v, isSample1)) =>
-        val (add1, add2) = if (isSample1) (1, 0) else (0, 1)
-        val cdf1 = Math.max(acc.ix1 + add1, 0) / n1
-        val cdf2 = Math.max(acc.ix2 + add2, 0) / n2
-        val dist = cdf1 - cdf2
-        KS2Acc(Math.min(acc.min, dist), Math.max(acc.max, dist), acc.ix1 + add1, acc.ix2 + add2)
+    val (add1, add2) = if (isSample1) (1, 0) else (0, 1)
+    val cdf1 = Math.max(acc.ix1 + add1, 0) / n1
+    val cdf2 = Math.max(acc.ix2 + add2, 0) / n2
+    val dist = cdf1 - cdf2
+    KS2Acc(Math.min(acc.min, dist), Math.max(acc.max, dist), acc.ix1 + add1, acc.ix2 + add2)
     }
     // min, max, ct sample 1, ct sample 2
     Array((results.min, results.max, (results.ix1 + 1) * n2  -  (results.ix2 + 1) * n1)).iterator
   }
 
+  /**
+   * Adjust candidate extremes by the appropriate constant. The resulting maximum corresponds to
+   * the 2-sample, 2-sided Kolmogorov Smirnov test
+   * @param localData `Array[(Double, Double, Double)]` contains the candidate extremes from each
+   *                 partition, along with the numerator for the necessary constant adjustments
+   * @param n `Double` The denominator in the constant adjustment (i.e. (size of sample 1 ) * (size
+   *         of sample 2)
+   * @return The 2-sample, 2-sided Kolmogorov Smirnov statistic
+   */
   private def searchTwoSampleStatistic(localData: Array[(Double, Double, Double)], n: Double)
     : Double = {
     val initAcc = (Double.MinValue, 0.0) // maximum distance and numerator for constant adjustment
     // adjust differences based on the # of elements preceding it, which should provide
     // the correct distance between the 2 ECDFs
     val results = localData.foldLeft(initAcc) { case ((prevMax, prevCt), (minCand, maxCand, ct)) =>
-        val adjConst = prevCt / n
-        val dist1 = Math.abs(minCand + adjConst)
-        val dist2 = Math.abs(maxCand + adjConst)
-        val maxVal = Array(prevMax, dist1, dist2).max
-        (maxVal, prevCt + ct)
+    val adjConst = prevCt / n
+    val dist1 = Math.abs(minCand + adjConst)
+    val dist2 = Math.abs(maxCand + adjConst)
+    val maxVal = Array(prevMax, dist1, dist2).max
+    (maxVal, prevCt + ct)
     }
     results._1
   }
